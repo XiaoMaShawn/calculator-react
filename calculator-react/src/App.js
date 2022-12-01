41217279df0fe86d87fbd5170f64b6ecea79458c
XiaoMaShawn <ma2014xiao@hotmail.com>
2022-11-30 20:37:52 -0500

    css style for calculator

--- calculator-react/src/App.js
+++ calculator-react/src/App.js
@@ -1,11 +1,11 @@
-// import './App.css';
+import './App.css';
 
 function App() {
   return (
-    <div className="calculated-grid">
+    <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand"></div>
-        <div className="current-operand"></div>
+        <div className="previous-operand">123</div>
+        <div className="current-operand">456</div>
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
